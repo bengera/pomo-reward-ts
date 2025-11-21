1f571d7434e2343f12a3c6b9ccbaa1cd655bccd6
Ben <61493854+bengera@users.noreply.github.com>
2025-11-21 16:05:55 +0700

    Change setter types to shorthand Dispatch

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,20 +1,28 @@
+import { Dispatch, SetStateAction } from "react";
+
 type Reward = {
   id: string;
   description: string;
   category: "reward";
   price: number;
 };
+// Reacts own types hide arrow functions and void for you
+//(value: number | ((prev: number) => number)) => void -
+// shorthand
+//  Dispatch<SetStateAction<Reward[]>>
 
 export type RewardListProps = {
   rewards: Reward[];
-  setRewards: (value: Reward[] | ((prev: Reward[]) => Reward[])) => void;
-  setMoney: (value: number | ((prev: number) => number)) => void;
-  setRewardsCounter: (value: number | ((prev: number) => number)) => void;
+  setRewards: Dispatch<SetStateAction<Reward[]>>; //void by default
+  setMoney: Dispatch<SetStateAction<number>>;
+  setRewardsCounter: Dispatch<SetStateAction<number>>;
+
   money: number;
   timerRunning: boolean;
   timeLeft: number;
-  setOverlay: (value: boolean) => void;
-  setCurrentClaim: (value: Reward) => void;
+
+  setOverlay: Dispatch<SetStateAction<boolean>>;
+  setCurrentClaim: Dispatch<SetStateAction<Reward[]>>;
   allQuotes: string[];
-  setSelectedQuote: (value: string) => void;
+  setSelectedQuote: Dispatch<SetStateAction<string>>;
 };
